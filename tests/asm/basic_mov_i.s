9b81e7633d544583262ae80cc4f0476687d6da7a
siliscale <info@siliscale.com>
2025-11-26 16:36:28 +0000

    Added mov immediate testcase

---
+++ tests/asm/basic_mov_i.s
@@ -0,0 +1,14 @@
+        .text
+        .globl  _start
+_start:                                   
+        r1 = 1
+        r1 = 2
+        r1 = 3
+        r1 = 4
+        r1 = 5
+        r1 = 6
+        r1 = 7
+        r1 = 8
+        r1 = 9
+        r1 = 10
+        exit
